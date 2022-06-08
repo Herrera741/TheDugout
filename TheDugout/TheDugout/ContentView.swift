5638080676a27add164cf9f6e494e081fcb30dde
Sergio <sherrera725@gmail.com>
2022-06-07 22:21:02 -0700

    Init project setup done

---
+++ TheDugout/TheDugout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheDugout
+//
+//  Created by Sergio Herrera on 6/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheDugout/TheDugout/TheDugoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheDugoutApp.swift
+//  TheDugout
+//
+//  Created by Sergio Herrera on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct TheDugoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
